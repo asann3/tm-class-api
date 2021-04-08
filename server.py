42c701fd5b5bbec58ea0b91ec9d02d9e21cf69b9
asann3 <39186984+asann3@users.noreply.github.com>
2021-04-09 00:17:39 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
@@ -8,30 +9,52 @@ app.config["JSON_SORT_KEYS"] = False  # ソートをそのまま
 
 @app.route('/', methods=['GET'])
 def index():
-    # a = xbhiugvcfdrtybiugcdrtyhinucdrtyyesxrtcdyijmokzawsxlopazwlpazlopsxwkolsxecdijmkdcrfvtbgyhasfdgxcfhhhhhhhhhhhhhhhtw3erawerfsrt4653wssdfdsvvbbbntehbbbbbbbbbbbb
     return "Hello Flask Test docker"
+
 
 # 範囲外(7, 8などがない場合や, 9時間目以上)
 
-# formatterが改行してくれないので直す
 
-
-@app.route('/classes/1/<dayOfWeek>')
+@app.route('/classes/1/<dayOfWeek>', methods=['GET'])
 def foo(dayOfWeek):
-    if dayOfWeek == "Mon":
-        data = {1: "応用物理i", 2: "応用物理i",
-                3: "ソフトウェア工学", 4: "ソフトウェア工学",
-                5: "ソフトウェアデザイン演習ii", 6: "ソフトウェアデザイン演習ii"}
-    if dayOfWeek == "Tue":
-        data = {1: "専門選択", 2: "専門選択", 3: "一般選択", 4: "一般選択", 5: "ビジネスi", 6: "ビジネスi"}
-    if dayOfWeek == "Wed":
-        data = {1: "英語ivC", 2: "英語ivC", 3: "ハードウェア総論", 4: "ハードウェア総論", 5: "応用数学i", 6: "応用数学i", 7: "HR"}
-    if dayOfWeek == "Thu":
-        data = {1: "回路理論ii", 2: "回路理論ii", 5: "情報科学・工学実験", 6: "情報科学・工学実験"}
-    if dayOfWeek == "Fri":
-        data = {1: "データベース", 2: "データベース", 3: "情報数学", 4: "情報数学"}
+    if request.method == 'GET':
+        if dayOfWeek == "Mon":
+            data = {
+                1: "応用物理i",
+                2: "応用物理i",
+                3: "ソフトウェア工学",
+                4: "ソフトウェア工学",
+                5: "ソフトウェアデザイン演習ii",
+                6: "ソフトウェアデザイン演習ii"
+            }
+        if dayOfWeek == "Tue":
+            data = {
+                1: "専門選択",
+                2: "専門選択",
+                3: "一般選択",
+                4: "一般選択",
+                5: "ビジネスi",
+                6: "ビジネスi"
+            }
+        if dayOfWeek == "Wed":
+            data = {
+                1: "英語ivC",
+                2: "英語ivC",
+                3: "ハードウェア総論",
+                4: "ハードウェア総論",
+                5: "応用数学i",
+                6: "応用数学i",
+                7: "HR"
+            }
+        if dayOfWeek == "Thu":
+            data = {1: "回路理論ii", 2: "回路理論ii", 5: "情報科学・工学実験", 6: "情報科学・工学実験"}
+        if dayOfWeek == "Fri":
+            data = {1: "データベース", 2: "データベース", 3: "情報数学", 4: "情報数学"}
+        # else:
+        #     data =
 
     return jsonify({'status': 'OK', 'body': data})
+
 
 if __name__ == "__main__":
     app.debug = True
