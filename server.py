0451896e871271e358cc6fb9dc1143f06b9fe43f
asann3 <39186984+asann3@users.noreply.github.com>
2021-04-09 20:45:18 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+# , Response
+# import json
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -13,47 +15,130 @@ def index():
 
 
 # 範囲外(7, 8などがない場合や, 9時間目以上)
+# 教員名, apiのversion, 変更しやすさ
 
 
-@app.route('/classes/1/<dayOfWeek>', methods=['GET'])
+@app.route('/classes/4/5/<dayOfWeek>', methods=['GET'])
 def foo(dayOfWeek):
     if request.method == 'GET':
         if dayOfWeek == "Mon":
-            data = {
-                1: "応用物理i",
-                2: "応用物理i",
-                3: "ソフトウェア工学",
-                4: "ソフトウェア工学",
-                5: "ソフトウェアデザイン演習ii",
-                6: "ソフトウェアデザイン演習ii"
-            }
+            data = [{
+                "time": 1,
+                "subject": "応用物理i"
+            }, {
+                "time": 2,
+                "subject": "応用物理i"
+            }, {
+                "time": 3,
+                "subject": "ソフトウェア工学"
+            }, {
+                "time": 4,
+                "subject": "ソフトウェア工学"
+            }, {
+                "time": 5,
+                "subject": "ソフトウェアデザイン演習ii"
+            }, {
+                "time": 6,
+                "subject": "ソフトウェアデザイン演習ii"
+            }]
+
         if dayOfWeek == "Tue":
-            data = {
-                1: "専門選択",
-                2: "専門選択",
-                3: "一般選択",
-                4: "一般選択",
-                5: "ビジネスi",
-                6: "ビジネスi"
-            }
+            data = [{
+                "time": 1,
+                "subject": "専門選択"
+            }, {
+                "time": 2,
+                "subject": "専門選択"
+            }, {
+                "time": 3,
+                "subject": "一般選択"
+            }, {
+                "time": 4,
+                "subject": "一般選択"
+            }, {
+                "time": 5,
+                "subject": "ビジネスi"
+            }, {
+                "time": 6,
+                "subject": "ビジネスi"
+            }]
+
         if dayOfWeek == "Wed":
-            data = {
-                1: "英語ivC",
-                2: "英語ivC",
-                3: "ハードウェア総論",
-                4: "ハードウェア総論",
-                5: "応用数学i",
-                6: "応用数学i",
-                7: "HR"
-            }
+            data = [{
+                "time": 1,
+                "subject": "英語ivC"
+            }, {
+                "time": 2,
+                "subject": "英語ivC"
+            }, {
+                "time": 3,
+                "subject": "ハードウェア総論"
+            }, {
+                "time": 4,
+                "subject": "ハードウェア総論"
+            }, {
+                "time": 5,
+                "subject": "応用数学i"
+            }, {
+                "time": 6,
+                "subject": "応用数学i"
+            }, {
+                "time": 7,
+                "subject": "HR"
+            }]
+
         if dayOfWeek == "Thu":
-            data = {1: "回路理論ii", 2: "回路理論ii", 5: "情報科学・工学実験", 6: "情報科学・工学実験"}
+            data = [{
+                "time": 1,
+                "subject": "回路理論ii"
+            }, {
+                "time": 2,
+                "subject": "回路理論ii"
+            }, {
+                "time": 5,
+                "subject": "情報科学・工学実験iii"
+            }, {
+                "time": 6,
+                "subject": "情報科学・工学実験iii"
+            }, {
+                "time": 7,
+                "subject": "情報科学・工学実験iii"
+            }]
         if dayOfWeek == "Fri":
-            data = {1: "データベース", 2: "データベース", 3: "情報数学", 4: "情報数学"}
+            data = [{
+                "time": 1,
+                "subject": "データベース"
+            }, {
+                "time": 2,
+                "subject": "データベース"
+            }, {
+                "time": 3,
+                "subject": "情報数学"
+            }, {
+                "time": 4,
+                "subject": "情報数学"
+            }]
         # else:
         #     data =
+    return jsonify({'status': 'OK', 'body': data}), 200
 
-    return jsonify({'status': 'OK', 'body': data})
+    # return Response(response=json.dumps({
+    #     'status': 'OK',
+    #     'body': data
+    # }),
+    #                 status=200)
+
+
+@app.errorhandler(400)
+@app.errorhandler(404)
+def error_handler(error):
+    return jsonify
+    ({
+        'error': {
+            'code': error.description['code'],
+            'message': error.description['message']
+        }
+    }), error.code
 
 
 if __name__ == "__main__":
