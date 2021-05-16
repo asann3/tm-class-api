103b9dd09e7da1c361c8596f6a6836d55ceb9bc5
asann3 <39186984+asann3@users.noreply.github.com>
2021-05-16 16:17:00 +0900

    Update files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 import json
-# import os
+import datetime as dt
+
+# import format_csv
+from format_csv import gen_changed_data
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -9,122 +12,246 @@ json_open = open('data.json', 'r')
 json_load = json.load(json_open)
 
 
+def date_to_weekday(dates):
+    year = int(dates[:4])
+    month = int(dates[4:6])
+    day = int(dates[6:])
+    weekday = dt.date(year, month, day).strftime('%a')
+    return weekday
+
+
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello Flask Test docker"
+    return "苫小牧高専授業変更情報API"
 
 
-# 範囲外(7, 8などがない場合や, 9時間目以上)
-# 教員名, apiのversion, 変更しやすさ
-# ルートディレクトリに何を配置するか
-# エラーハンドリング
-# urlのフィールド
-# エンドポイントのスラッシュ 有無
-
-
-@app.route('/api/v1/classes/<grade>/<course>/<dayOfWeek>', methods=['GET'])
-def foo(grade, course, dayOfWeek):
+@app.route('/api/v1/classes/<grade>/<course>/<dates>', methods=['GET'])
+def foo(grade, course, dates):
+    date_list, grade_list, course_list, time_period_list, subjects_after_change, data_length = gen_changed_data(
+    )
+    grade = str(grade)
+    course = str(course)
+    dates = str(dates)
+    # flag = {"a": type(grade), "b": type(course), "c": type(dates)}
+    change = data_length
+    returnval = []
+    dayOfWeek = date_to_weekday(dates)
     if request.method == 'GET':
         if dayOfWeek == "Mon":
-            data = [{
-                "time": 1,
-                "subject": "応用物理i"
-            }, {
-                "time": 2,
-                "subject": "応用物理i"
-            }, {
-                "time": 3,
-                "subject": "ソフトウェア工学"
-            }, {
-                "time": 4,
-                "subject": "ソフトウェア工学"
-            }, {
-                "time": 5,
-                "subject": "ソフトウェアデザイン演習ii"
-            }, {
-                "time": 6,
-                "subject": "ソフトウェアデザイン演習ii"
-            }]
+            data = {
+                "1": {
+                    "time": 1,
+                    "subject": "応用物理i"
+                },
+                "2": {
+                    "time": 2,
+                    "subject": "応用物理i"
+                },
+                "3": {
+                    "time": 3,
+                    "subject": "ソフトウェア工学"
+                },
+                "4": {
+                    "time": 4,
+                    "subject": "ソフトウェア工学"
+                },
+                "5": {
+                    "time": 5,
+                    "subject": "ソフトウェアデザイン演習ii"
+                },
+                "6": {
+                    "time": 6,
+                    "subject": "ソフトウェアデザイン演習ii"
+                },
+                "7": {
+                    "time": 7,
+                    "subject": ""
+                },
+                "8": {
+                    "time": 8,
+                    "subject": ""
+                }
+            }
 
-        if dayOfWeek == "Tue":
-            data = [{
-                "time": 1,
-                "subject": "専門選択"
-            }, {
-                "time": 2,
-                "subject": "専門選択"
-            }, {
-                "time": 3,
-                "subject": "一般選択"
-            }, {
-                "time": 4,
-                "subject": "一般選択"
-            }, {
-                "time": 5,
-                "subject": "ビジネスi"
-            }, {
-                "time": 6,
-                "subject": "ビジネスi"
-            }]
+        elif dayOfWeek == "Tue":
+            data = {
+                "1": {
+                    "time": 1,
+                    "subject": "専門選択"
+                },
+                "2": {
+                    "time": 2,
+                    "subject": "専門選択"
+                },
+                "3": {
+                    "time": 3,
+                    "subject": "一般選択"
+                },
+                "4": {
+                    "time": 4,
+                    "subject": "一般選択"
+                },
+                "5": {
+                    "time": 5,
+                    "subject": "ビジネスi"
+                },
+                "6": {
+                    "time": 6,
+                    "subject": "ビジネスi"
+                },
+                "7": {
+                    "time": 7,
+                    "subject": ""
+                },
+                "8": {
+                    "time": 8,
+                    "subject": ""
+                }
+            }
 
-        if dayOfWeek == "Wed":
-            data = [{
-                "time": 1,
-                "subject": "英語ivC"
-            }, {
-                "time": 2,
-                "subject": "英語ivC"
-            }, {
-                "time": 3,
-                "subject": "ハードウェア総論"
-            }, {
-                "time": 4,
-                "subject": "ハードウェア総論"
-            }, {
-                "time": 5,
-                "subject": "応用数学i"
-            }, {
-                "time": 6,
-                "subject": "応用数学i"
-            }, {
-                "time": 7,
-                "subject": "HR"
-            }]
+        elif dayOfWeek == "Wed":
+            data = {
+                "1": {
+                    "time": 1,
+                    "subject": "英語ivC"
+                },
+                "2": {
+                    "time": 2,
+                    "subject": "英語ivC"
+                },
+                "3": {
+                    "time": 3,
+                    "subject": "ハードウェア総論"
+                },
+                "4": {
+                    "time": 4,
+                    "subject": "ハードウェア総論"
+                },
+                "5": {
+                    "time": 5,
+                    "subject": "応用数学i"
+                },
+                "6": {
+                    "time": 6,
+                    "subject": "応用数学i"
+                },
+                "7": {
+                    "time": 7,
+                    "subject": "HR"
+                },
+                "8": {
+                    "time": 8,
+                    "subject": ""
+                }
+            }
 
-        if dayOfWeek == "Thu":
-            data = [{
-                "time": 1,
-                "subject": "回路理論ii"
-            }, {
-                "time": 2,
-                "subject": "回路理論ii"
-            }, {
-                "time": 5,
-                "subject": "情報科学・工学実験iii"
-            }, {
-                "time": 6,
-                "subject": "情報科学・工学実験iii"
-            }, {
-                "time": 7,
-                "subject": "情報科学・工学実験iii"
-            }]
-        if dayOfWeek == "Fri":
-            data = [{
-                "time": 1,
-                "subject": "データベース"
-            }, {
-                "time": 2,
-                "subject": "データベース"
-            }, {
-                "time": 3,
-                "subject": "情報数学"
-            }, {
-                "time": 4,
-                "subject": "情報数学"
-            }]
-        # else:
-        #     data =
-        result = {'status': 'OK', 'data': data}
+        elif dayOfWeek == "Thu":
+            data = {
+                "1": {
+                    "time": 1,
+                    "subject": "回路理論ii"
+                },
+                "2": {
+                    "time": 2,
+                    "subject": "回路理論ii"
+                },
+                "3": {
+                    "time": 3,
+                    "subject": ""
+                },
+                "4": {
+                    "time": 4,
+                    "subject": ""
+                },
+                "5": {
+                    "time": 5,
+                    "subject": "情報科学・工学実験iii"
+                },
+                "6": {
+                    "time": 6,
+                    "subject": "情報科学・工学実験iii"
+                },
+                "7": {
+                    "time": 7,
+                    "subject": "情報科学・工学実験iii"
+                },
+                "8": {
+                    "time": 8,
+                    "subject": ""
+                }
+            }
+        elif dayOfWeek == "Fri":
+            data = {
+                "1": {
+                    "time": 1,
+                    "subject": "データベース"
+                },
+                "2": {
+                    "time": 2,
+                    "subject": "データベース"
+                },
+                "3": {
+                    "time": 3,
+                    "subject": "情報数学"
+                },
+                "4": {
+                    "time": 4,
+                    "subject": "情報数学"
+                },
+                "5": {
+                    "time": 5,
+                    "subject": ""
+                },
+                "6": {
+                    "time": 6,
+                    "subject": ""
+                },
+                "7": {
+                    "time": 7,
+                    "subject": ""
+                },
+                "8": {
+                    "time": 8,
+                    "subject": ""
+                }
+            }
+        # if dayOfWeek == "Sat":
+        #     data = [{"今日は": "土曜日です"}]
+        # if dayOfWeek == "Sun":
+        #     data = [{"今日は": "日曜日です"}]
+        else:
+            data = {"holiday": "Today is a holiday!!!", "dayOfWeek": dayOfWeek}
+        # j = json.loads(data)
+
+        if dates in date_list:
+            # index = format_csv.time_period_list[0]
+            # flag = int(format_csv.line_count)
+            # ['subject']
+            # returnval.append({'len': data_length})
+            for row in range(data_length):
+
+                returnval.append({
+                    # 'row': row,
+                    "time": time_period_list[row],
+                    'date': date_list[row]
+                })
+                if dates == date_list[row] and course == course_list[
+                        row] and grade == grade_list[row]:
+                    data[time_period_list[row]][
+                        "subject"] = subjects_after_change[row]
+                    # change = 1
+                    # flag = 1
+            # print(format_csv.grade[row])
+            # data[str(
+            #     format_csv.time_period_list
+            # )]['subject'] = format_csv.subjects_after_change[row]
+        # data[times][subject] = new_data
+        result = {
+            'status': 'OK',
+            'data': data,
+            'changed': change,
+            'return': returnval
+        }
     return jsonify(result), 200
 
 
--- format_csv.py
+++ format_csv.py
@@ -1,4 +1,3 @@
-# from numpy import nan
 import pandas as pd
 import datetime as dt
 
@@ -7,18 +6,20 @@ df = pd.read_csv(fileName)
 line_count = df.count()[0]  # 行数
 df = df.fillna('')
 
-grade_list = []
-course_list = []
+# grade_list = []
+# course_list = []
 subjects_before_change = []
 subjects_after_change = []
 date_list = []
 weekday_list = []
 time_period_list = []
-print(df)
+data_length = 0
+# print(df)
 
 
-def append_data():
-    date_list.append(year + month + day)
+def append_data(row, year, month, day, grade, course, grade_list, course_list,
+                subjects_before_change, subjects_after_change, weekday_list):
+    # date_list.append(year + month + day)
     weekday_list.append(dt.date(year, month, day).strftime('%a'))
     subjects_after_change.append(df['変更後科目'][row])
     subjects_before_change.append(df['変更前科目'][row])
@@ -30,20 +31,59 @@ def append_data():
     else:
         grade_list.append(grade)
         course_list.append(course)
+    # print(grade_list)
 
 
-for row in range(line_count):
-    date_list.append((df['日付'][row]).replace('/', ''))
-    year = int(df['日付'][row][:4])
-    month = int(df['日付'][row][5:7])
-    day = int(df['日付'][row][8:10])
-    grade = df['クラス'][row][0]
-    course = df['クラス'][row][2]
-    print(df['変更前科目'][row])
-    print(type(df['変更前科目'][row]))
+def gen_changed_data():
+    # 別の場所で関数を呼び出したとしても常に値が同じになるように初期化
+    date_list = []
+    grade_list = []
+    course_list = []
+    time_period_list = []
+    subjects_before_change = []
+    subjects_after_change = []
+    weekday_list = []
+    data_length = 0
+    for row in range(line_count):
+        date_list.append((df['日付'][row]).replace('/', ''))
+        year = int(df['日付'][row][:4])
+        month = int(df['日付'][row][5:7])
+        day = int(df['日付'][row][8:10])
+        grade = df['クラス'][row][0]
+        course = df['クラス'][row][2]
 
-    append_data()
-    time_period_list.append(df['時限'][row][0])
-    if len(str(df['時限'][row])) == 3:
-        append_data()
+        append_data(row, year, month, day, grade, course, grade_list,
+                    course_list, subjects_before_change, subjects_after_change,
+                    weekday_list)
         time_period_list.append(df['時限'][row][0])
+        if len(str(df['時限'][row])) == 3:
+            # print('a')
+            # print(grade_list)
+            append_data(row, year, month, day, grade, course, grade_list,
+                        course_list, subjects_before_change,
+                        subjects_after_change, weekday_list)
+            time_period_list.append(df['時限'][row][2])
+            date_list.append((df['日付'][row]).replace('/', ''))
+            # print(time_period_list)
+            # print(grade_list)
+        # print(grade_list)
+    # print(date_list)
+    # print(type(time_period_list[0]))
+    # for i in range(line_count):
+    #     print(i, date_list[i], time_period_list[i])
+    data_length = len(date_list)
+    # print(grade_list)
+    print(len(date_list), len(grade_list), len(course_list),
+          len(time_period_list), len(subjects_after_change), data_length)
+    return date_list, grade_list, course_list, time_period_list, subjects_after_change, data_length
+
+
+# print(gen_changed_data)
+# date_list, grades_list, course_list, time_period_list, subjects_after_change, data_length = gen_changed_data(
+# )
+gen_changed_data()
+# print(len(date_list))
+# print(grades_list)
+# for i in range(line_count):
+#     print(i, date_list[i], time_period_list[i])
+# print(time_period_list)
